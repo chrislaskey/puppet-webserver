f4dff18b2cb8f7f19d6ab1c82e0b14016ec42e86
Chris Laskey <contact@chrislaskey.com>
2013-03-28 17:05:32 -0400

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,190 @@
+define website (
+	$project_name = $title,
+	$server_name = "",
+	$project_type = "",
+	$server_alias = "",
+	$webserver = "apache",
+	$webserver_ports = "80", # Can specify multiples using a list: ["80", "8080"]
+	$additional_webserver_config = "",
+	$websites_dir = "/data/websites",
+	$backup = True,
+	$backup_remote_server = $puppetmaster_fqdn,
+	$backup_remote_dir = "/data/available-websites",
+	$backup_user = "deploy",
+	$backup_remote_ssh_port = "2222",
+	$backup_days = '6',
+	$backup_weeks = '5',
+	$backup_months = '6',
+	$backup_years = '20',
+	$backup_cron_minute = '0',
+	$backup_cron_hour = 2, # Can be individual: [2,12,15] or ranged: [2-4,10-14]
+	$backup_cron_weekday = '*',
+	$backup_cron_monthday = '*',
+	$backup_cron_month = '*',
+) {
+
+	# Class variables
+	# ==========================================================================
+
+	$project_path = "${websites_dir}/${project_name}"
+
+	# Transfer project code
+	# ==========================================================================
+
+	if ! defined( File[$websites_dir] ){
+		file { $websites_dir:
+			ensure => "directory",
+			owner => "www-data",
+			group => "www-data",
+			mode => "0775",
+		}
+	}
+
+	# Deploy webserver configuration
+	# ==========================================================================
+
+	if $webserver == "apache" {
+
+		if $project_type == "" {
+			fail("The 'project_type' attribute can not be empty if using Apache webserver. Check website definitions in nodes.pp.")
+		}
+
+		if $server_name == "" {
+			fail("The 'server_name' attribute can not be empty if using Apache webserver. Check website definitions in nodes.pp.")
+		}
+
+		$apache_config_available_dir = "/etc/apache2/sites-available/${project_name}"
+		$apache_config_enabled_dir = "/etc/apache2/sites-enabled/${project_name}"
+
+		case $project_type {
+			"python": {
+				$site_wsgi_file = "${project_path}/${project_name}.wsgi"
+				$virtualenv_site_packages = "${project_path}/.meta/virtualenv/lib/python2.7/site-packages"
+			}
+			"php": {
+
+			}
+		}
+
+		file { $apache_config_available_dir:
+			ensure => "present",
+			content => template("website/apache-site-${project_type}"),
+			owner => "root",
+			group => "root",
+			mode => "0644",
+			notify => [
+				Exec["apache2-config-reloader-${project_name}"],
+			],
+		}
+
+		file { $apache_config_enabled_dir:
+			ensure => "link",
+			target => $apache_config_available_dir,
+			require => [
+				File[$apache_config_available_dir],
+			],
+			notify => [
+				Exec["apache2-config-reloader-${project_name}"],
+				Exec["${project_name}-deploy.sh"],
+			],
+		}
+
+		if ! defined(File["/etc/apache2/sites-enabled/000-default"]) {
+			file { "/etc/apache2/sites-enabled/000-default":
+				ensure => "absent",
+			}
+		}
+
+		exec { "apache2-config-reloader-${project_name}":
+			command => "service apache2 reload",
+			path => "/bin:/sbin:/usr/bin:/usr/sbin",
+			user => "root",
+			group => "root",
+			logoutput => "on_failure",
+			refreshonly => "true",
+			subscribe => [
+				File[$apache_config_enabled_dir],
+			],
+			require => [
+				File["/etc/apache2/sites-enabled/000-default"],
+			],
+		}
+
+	}
+
+	# Website deployment and configuration
+	# ==========================================================================
+
+	if ! defined( File["${websites_dir}/deploy.sh"] ) {
+		file { "${websites_dir}/deploy.sh":
+			ensure => "present",
+			content => template("website/deploy-project.sh"),
+			owner => "root",
+			group => "root",
+			mode => "0700",
+			require => [
+				File["${websites_dir}"],
+			],
+		}
+	}
+
+	exec { "${project_name}-deploy.sh":
+		command => "${websites_dir}/deploy.sh ${project_name}",
+		path => "/bin:/sbin:/usr/bin:/usr/sbin",
+		user => "root",
+		group => "root",
+		logoutput => "on_failure",
+	}
+
+	if $backup == True {
+
+		if ! defined( File["/data/puppet/pre-backup"] ) {
+			file { "/data/puppet/pre-backup":
+				ensure => "directory",
+				owner => "root",
+				group => "root",
+				mode => "0700",
+			}
+		}
+
+		if ! defined( File["/data/puppet/pre-backup/pre-backup.sh"] ) {
+			file { "/data/puppet/pre-backup/pre-backup.sh":
+				ensure => "present",
+				source => "puppet:///modules/website/pre-backup.sh",
+				owner => "root",
+				group => "root",
+				mode => "0700",
+			}
+		}
+
+		if ! defined( File["/data/puppet/backup"] ) {
+			file { "/data/puppet/backup":
+				ensure => "present",
+				source => "puppet:///modules/website/backup", # The 'files' dir is omitted
+				recurse => true, # Transfer directory files too
+				purge => true, # Remove client files not found on puppetmaster dir
+				force => true, # Remove client dirs not found on puppetmaster dir
+				owner => "root",
+				group => "root",
+				mode => "0700",
+				require => [
+					File["/data/puppet"],
+				],
+			}
+		}
+
+		cron { "${project_name}-backup-cron":
+			command => "/data/puppet/pre-backup/pre-backup.sh -p ${project_path}; /data/puppet/backup/roundrobinbackup.py ${project_path}/ ${backup_user}@${backup_remote_server}:${backup_remote_dir}/${project_name}/backup --ssh-port ${backup_remote_ssh_port} --ssh-identity-file /home/${backup_user}/.ssh/${backup_remote_server} --days ${backup_days} --weeks ${backup_weeks} --months ${backup_months} --years ${backup_years} --exclude .git*",
+			user => root,
+			minute => $backup_cron_minute,
+			hour => $backup_cron_hour,
+			weekday => $backup_cron_weekday,
+			monthday => $backup_cron_monthday,
+			month => $backup_cron_month,
+			require => [
+				File["/data/puppet/backup"],
+			],
+		}
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,108 @@
+About
+================================================================================
+
+A custom puppet module for deploying websites to Ubuntu webservers. This module
+is a meta-module, combining many projects such as deployment, backup, and
+Puppet configuration scripts under one umbrella module.
+
+Default arguments for 0.8.0 include:
+
+		$project_name = $title,
+		$server_name = "",
+		$project_type = "",
+		$server_alias = "",
+		$webserver = "apache",
+		$webserver_ports = "80", # Can specify multiples using a list: ["80", "8080"]
+		$additional_webserver_config = "",
+		$websites_dir = "/data/websites",
+		$backup = True,
+		$backup_remote_server = $puppetmaster_fqdn,
+		$backup_remote_dir = "/data/available-websites",
+		$backup_user = "deploy",
+		$backup_remote_ssh_port = "2222",
+		$backup_days = '6',
+		$backup_weeks = '5',
+		$backup_months = '6',
+		$backup_years = '20',
+		$backup_cron_minute = '0',
+		$backup_cron_hour = 2, # Can be individual: [2,12,15] or ranged: [2-4,10-14]
+		$backup_cron_weekday = '*',
+		$backup_cron_monthday = '*',
+		$backup_cron_month = '*',
+
+The webserver parameter currently supports Apache configurations for
+`Apache+PHP_Fastcgi` and `Apache+Mod_WSGI`.
+
+Deploy Script
+-------------
+
+This module includes a project deployment shell script that:
+
+	- Transfers files intelligently from a mix of live-data backups and
+	  git repository files.
+	- Sets up PHP and Python environments, including `setup.sh` and `setup.sh`
+	  scripts.
+	- Creates MySQL database, including database creation, user creation,
+	  schema checks, and data importing.
+
+Future revisions will include post-install smoke tests and SQLite support.
+
+# Server Filesystem #
+
+Available projects are stored on the puppet server in the following configuration:
+
+	available-websites/my-project
+	available-websites/my-project/files/ # Git repository or flat files
+	available-websites/my-project/backup/
+	available-websites/my-project/backup/latest/
+	available-websites/my-project/backup/automated-backup-2013-01-01.tar.gz
+	available-websites/my-project/backup/automated-backup-2012-01-01.tar.gz
+
+Inside the `my-project/files` directory should be a current group of files
+and/or a git repository of files.
+
+The backup script will create the `backup/`, put the latest files in
+`backup/latest`, and create archives based on a round-robin backup scheme and
+Puppet parameters passed to the class.
+
+# Project Filesystem #
+
+The project files should include a `.config` directory. This contains meta
+information about the project, including information on how to deploy. It may
+contains directories such as:
+
+	.config/
+	.config/mysql-appusers/
+	.config/mysql-appdata/
+	.config/scripts/
+
+Where a `mysql-*` directory includes the filels:
+
+	.config/mysql-appusers/database.sql
+	.config/mysql-appusers/name
+	.config/mysql-appusers/schema.sql
+	.config/mysql-appusers/users.sql
+
+**Note:** If there is only one database, then the dir can simply be called
+`mysql`.
+
+The `scripts/` directory stores pre-deploy, post-deploy, and post-deploy smoke
+test scripts.
+
+Backup Script
+-------------
+
+A custom puppet-compatable push-based round-robin automated file backup system
+is included. See the
+[round-robin-backup.py](https://github.com/chrislaskey/round-robin-backup.py)
+repository for more information on the backup script, and see above for backup
+related passable class parameters in Puppet.
+
+Also includes a `pre-backup.sh` script for creating live-data information
+backups, such as MySQL database backups, before the round robin backup is run.
+
+License
+================================================================================
+
+All code written by me is released under MIT license. See the attached
+license.txt file for more information, including commentary on license choice.
